e233f5c56dd84bc810a04c9a8cfbb3afac552d3c
L0Z1K <qortmddbs731@naver.com>
2022-10-31 19:48:16 +0900

    test: users service

--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,3 +1,8 @@
+import {
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
@@ -26,4 +31,77 @@ describe('UsersService', () => {
       });
     });
   });
+
+  describe('getOne', () => {
+    it('should return a user', () => {
+      service.create({
+        name: 'Test User',
+        age: 20,
+      });
+      const user = service.getOne(1);
+      expect(user).toBeDefined();
+      expect(user.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('The user is not found.');
+      }
+    });
+
+    // I can't give string argument.
+    // it('should throw 400 error', () => {
+    //   try {
+    //     service.getOne('abc');
+    //   } catch (e) {
+    //     expect(e).toBeInstanceOf(BadRequestException);
+    //     expect(e.message).toEqual('Invalid user id');
+    //   }
+    // });
+  });
+
+  describe('create', () => {
+    it('should create a user', () => {
+      const beforeUsers = service.getAll().length;
+      service.create({
+        name: 'Test User',
+        age: 20,
+      });
+      const afterUsers = service.getAll().length;
+      expect(afterUsers).toEqual(beforeUsers + 1);
+    });
+
+    it('should throw 400 error because of empty name', () => {
+      try {
+        service.create({
+          name: '',
+          age: 20,
+        });
+      } catch (e) {
+        expect(e).toBeInstanceOf(BadRequestException);
+        expect(e.message).toEqual('name parameter is empty.');
+      }
+    });
+
+    it('should throw 409 error because name already exists', () => {
+      service.create({
+        name: 'Test User',
+        age: 20,
+      });
+      try {
+        service.create({
+          name: 'Test User',
+          age: 30,
+        });
+      } catch (e) {
+        expect(e).toBeInstanceOf(ConflictException);
+        expect(e.message).toEqual('The user already exists.');
+      }
+    });
+  });
+
+  describe('update', () => {});
 });
