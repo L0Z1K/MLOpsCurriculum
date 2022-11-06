cb7bb897d3b4ca1c04bce33274342946a8bff3f5
L0Z1K <qortmddbs731@naver.com>
2022-11-06 21:04:42 +0900

    fix: Remove unnecessary error message

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,9 +1,9 @@
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class createUserDTO {
-  @IsString({ message: 'name parameter is empty.' })
+  @IsString()
   readonly name: string;
   @IsOptional()
-  @IsNumber(undefined, { message: 'age must be an integer.' })
+  @IsNumber()
   readonly age: number;
 }
