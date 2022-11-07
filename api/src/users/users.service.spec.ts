4fbb4e3e20e8115ca2d6c2372883ba717355e9f3
L0Z1K <qortmddbs731@naver.com>
2022-11-07 22:08:07 +0900

    test: usersService

--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,5 +1,17 @@
+import { User } from './user.entity';
+import {
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+
+const mockUsersRepo = {
+  find: jest.fn(),
+  findOneBy: jest.fn(),
+  save: jest.fn(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -8,7 +20,13 @@ describe('UsersService', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [],
       //   imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([User])],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: 'UserRepository',
+          useValue: mockUsersRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -18,123 +36,65 @@ describe('UsersService', () => {
     expect(service).toBeDefined();
   });
 
-  //   describe('getAll', () => {
-  //     it('should return an array', () => {
-  //       const result = service.getAll();
-  //       expect(result).toBeInstanceOf(Array);
-  //     });
-  //   });
+  describe('getAll', () => {
+    it('should return an array', async () => {
+      jest.spyOn(mockUsersRepo, 'find').mockResolvedValue([]);
+      const result = await service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
 
-  //   describe('getOne', () => {
-  //     it('should return a user', async () => {
-  //       service.create({
-  //         name: 'Test User',
-  //         age: 20,
-  //       });
-  //       const user = await service.getOne(1);
-  //       expect(user).toBeDefined();
-  //       expect(user.id).toEqual(1);
-  //     });
+  describe('getOne', () => {
+    it('should return a user', async () => {
+      jest.spyOn(mockUsersRepo, 'findOneBy').mockResolvedValue({
+        id: 1,
+        name: 'Test User',
+        age: 30,
+      });
+      const user = await service.getOne(1);
+      expect(user).toBeDefined();
+      expect(user.id).toEqual(1);
+    });
 
-  //     it('should throw 404 error', () => {
-  //       try {
-  //         service.getOne(999);
-  //       } catch (e) {
-  //         expect(e).toBeInstanceOf(NotFoundException);
-  //         expect(e.message).toEqual('The user is not found.');
-  //       }
-  //     });
+    it('should throw 404 error', async () => {
+      jest.spyOn(mockUsersRepo, 'findOneBy').mockResolvedValue(undefined);
+      expect(async () => await service.getOne(999)).rejects.toThrow(
+        new NotFoundException('The user is not found.'),
+      );
+    });
+  });
 
-  //     // I can't give string argument.
-  //     // it('should throw 400 error', () => {
-  //     //   try {
-  //     //     service.getOne('abc');
-  //     //   } catch (e) {
-  //     //     expect(e).toBeInstanceOf(BadRequestException);
-  //     //     expect(e.message).toEqual('Invalid user id');
-  //     //   }
-  //     // });
-  //   });
+  describe('create', () => {
+    it('should create a user', async () => {
+      jest.spyOn(mockUsersRepo, 'findOneBy').mockResolvedValue(undefined);
+      jest.spyOn(mockUsersRepo, 'find').mockResolvedValue([
+        {
+          id: 1,
+          name: 'Test User',
+          age: 30,
+        },
+      ]);
+      const afterUsers = await service.getAll();
+      expect(afterUsers.length).toEqual(1);
+    });
 
-  //   describe('create', () => {
-  //     it('should create a user', async () => {
-  //       const beforeUsers = await service.getAll();
-  //       service.create({
-  //         name: 'Test User',
-  //         age: 20,
-  //       });
-  //       const afterUsers = await service.getAll();
-  //       expect(afterUsers.length).toEqual(beforeUsers.length + 1);
-  //     });
+    it('should throw 409 error because name already exists', async () => {
+      jest.spyOn(mockUsersRepo, 'findOneBy').mockResolvedValue({
+        id: 1,
+        name: 'Test User',
+        age: 30,
+      });
+      expect(
+        async () =>
+          await service.create({
+            name: 'Test User',
+            age: 30,
+          }),
+      ).rejects.toThrow(new ConflictException('The user already exists.'));
+    });
+  });
 
-  //     it('should throw 400 error because of empty name', () => {
-  //       try {
-  //         service.create({
-  //           name: '',
-  //           age: 20,
-  //         });
-  //       } catch (e) {
-  //         expect(e).toBeInstanceOf(BadRequestException);
-  //         expect(e.message).toEqual('name parameter is empty.');
-  //       }
-  //     });
+  describe('update', () => {});
 
-  //     it('should throw 409 error because name already exists', () => {
-  //       service.create({
-  //         name: 'Test User',
-  //         age: 20,
-  //       });
-  //       try {
-  //         service.create({
-  //           name: 'Test User',
-  //           age: 30,
-  //         });
-  //       } catch (e) {
-  //         expect(e).toBeInstanceOf(ConflictException);
-  //         expect(e.message).toEqual('The user already exists.');
-  //       }
-  //     });
-  //   });
-
-  //   describe('update', () => {
-  //     it('should update a user', async () => {
-  //       service.create({
-  //         name: 'Test User',
-  //         age: 20,
-  //       });
-  //       service.update(1, { name: 'Updated Test User' });
-  //       const user = await service.getOne(1);
-  //       expect(user.name).toEqual('Updated Test User');
-  //     });
-
-  //     it('should throw 409 error', () => {
-  //       service.create({
-  //         name: 'Test User',
-  //         age: 20,
-  //       });
-  //       service.create({
-  //         name: 'Test User2',
-  //         age: 30,
-  //       });
-  //       try {
-  //         service.update(1, { name: 'Test User2' });
-  //       } catch (e) {
-  //         expect(e).toBeInstanceOf(ConflictException);
-  //         expect(e.message).toEqual('The user already exists.');
-  //       }
-  //     });
-  //   });
-
-  //   describe('delete', () => {
-  //     it('should delete a user', async () => {
-  //       service.create({
-  //         name: 'Test User',
-  //         age: 20,
-  //       });
-  //       const beforeUsers = (await service.getAll()).length;
-  //       service.delete(1);
-  //       const afterUsers = (await service.getAll()).length;
-  //       expect(afterUsers).toEqual(beforeUsers - 1);
-  //     });
-  //   });
+  describe('delete', () => {});
 });
