42ff75d44d81ad4aebec76796e6a51bcab104bc4
L0Z1K <qortmddbs731@naver.com>
2022-11-24 16:28:26 +0900

    test: ecs with locust

--- api/test/locustfile.py
+++ api/test/locustfile.py
@@ -1,5 +1,5 @@
 from locust import HttpUser, task, between
-
+from random import randint
 class User(HttpUser):
     wait_time = between(1, 5)
 
@@ -9,6 +9,6 @@ class User(HttpUser):
     
     @task
     def create_user(self):
-        with self.client.post("/users", json={"name": "test", "age": 13}, catch_response=True) as response:
+        with self.client.post("/users", json={"name": f"test_{randint(1, 100)}", "age": 13}, catch_response=True) as response:
             if response.status_code == 409:
                 response.success()
